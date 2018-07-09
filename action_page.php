9234c3354805ab72a0b0d2afcb926bd53385ef39
shagilsiddiqui <40637140+shagilsiddiqui@users.noreply.github.com>
2018-07-10 00:18:25 +0530

    hey bhagwan tu hai ab

--- action_page.php
+++ action_page.php
@@ -1,20 +1,20 @@
-<?php
+<? php
+$to = "shagil.aryan@gmail.com";
+$Subject = "Email from my website";
 $First_name= $_POST['firstname'];
 $last_name= $_POST['lastname'];
 $country= $_POST['country'];
-$message=$_POST['message'];
+$message= $_POST['message'];
 
-$email_from='myserver@data.com'
-$email_subject="naya mal aaya hai kuch";
-$email_body="firstname:$First_name \n"
+$email_from= 'myserver@data.com'
+$email_body= "firstname:$First_name \n"
 			 "lastname:$last_name \n"
 			 "User Email:$visitor_email \n"
 			 "message:$message \n";
 			 "country:$country \n";
 
-$to="shagil.aryan@gmail.com";
-$header="From:$email_from \r\n";
+$header= "From:$email_from \r\n";
 $header= "Reply-To:$visitor_email \r\n";
-mail($to,$email_subject,$email_body,$header);
-header("Location:contact.html");
+mail ($to, $Subject, $email_body, $header);
+echo "Email has been sent! Thank u $First_name";
 ?>
